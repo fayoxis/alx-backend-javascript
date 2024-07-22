650aed9d0fbdbf513cad775a86f72ba4edfbb1c2
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-07-22 12:49:19 +0100

    Update 0-constants.js

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,16 +1,15 @@
-// Define a function that returns the task string
 export function taskFirst() {
-  return 'I prefer const when I can.';
+  const task = 'I prefer const when I can.';
+  return task;
 }
 
-// Define a function that returns the last part of the string
 export function getLast() {
   return ' is okay';
 }
 
-// Define a function that combines the task string and the last part
 export function taskNext() {
   let combination = 'But sometimes let';
   combination += getLast();
+
   return combination;
 }
