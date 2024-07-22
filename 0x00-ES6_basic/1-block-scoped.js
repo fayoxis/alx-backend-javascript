1138716e2d7659bf1412d19d0ecbe8705c729312
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-07-22 13:21:59 +0100

    Update 1-block-scoped.js

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -6,6 +6,10 @@ export default function taskBlock(trueOrFalse) {
     const task = true; // eslint-disable-line no-unused-vars
     const task2 = false; // eslint-disable-line no-unused-vars
   }
+    else {
+    const task = false;
+    const task2 = true;
+  }
 
   return [task, task2];
 }
