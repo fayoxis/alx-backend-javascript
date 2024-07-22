fbdeebbb5dbf85365996af60b7288cabd5f25dd3
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-07-23 00:36:17 +0100

    Update 101-iterateThroughObject.js

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,7 +1,6 @@
 export default function iterateThroughObject(reportWithIterator) {
   const employees = [];
 
-  // eslint-disable-next-line
   for (const employee of reportWithIterator) {
     employees.push(employee);
   }
