189713794ac8427f690e69797869299c1880d785
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-07-22 14:01:41 +0100

    Update 100-createIteratorObject.js

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,9 +1,31 @@
 export default function createIteratorObject(report) {
-  return (function* _() {
-    for (const department of Object.values(report.allEmployees)) {
-      for (const employee of department) {
-        yield employee;
+  const employeesByDepartment = report.allEmployees;
+  const departments = Object.keys(employeesByDepartment);
+  let currentDepartmentIndex = 0;
+  let currentEmployeeIndex = 0;
+
+  return {
+    next() {
+      const currentDepartment = departments[currentDepartmentIndex];
+      const employees = employeesByDepartment[currentDepartment];
+
+      if (currentEmployeeIndex >= employees.length) {
+        currentEmployeeIndex = 0;
+        currentDepartmentIndex++;
       }
-    }
-  }());
+
+      if (currentDepartmentIndex >= departments.length) {
+        return { done: true };
+      }
+
+      const currentEmployee = employees[currentEmployeeIndex];
+      currentEmployeeIndex++;
+
+      return { value: currentEmployee, done: false };
+    },
+
+    [Symbol.iterator]() {
+      return this;
+    },
+  };
 }
