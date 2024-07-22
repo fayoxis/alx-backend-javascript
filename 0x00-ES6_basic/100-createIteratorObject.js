54f35d9c11556021818bb823795fb5428b9adcae
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-07-23 00:48:06 +0100

    Update 100-createIteratorObject.js

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -2,9 +2,10 @@ export default function createIteratorObject(report) {
   return {
     *[Symbol.iterator]() {
       for (const department in report.allEmployees) {
-        for (const employee of report.allEmployees[department]) {
-          yield employee;
-          
+        if (Object.prototype.hasOwnProperty.call(report.allEmployees, department)) {
+          for (const employee of report.allEmployees[department]) {
+            yield employee;
+          }
         }
       }
     }
