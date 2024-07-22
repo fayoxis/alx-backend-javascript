17e03a353434ab37cf5bf09b4404f3622b437f4c
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-07-23 00:40:02 +0100

    Update 101-iterateThroughObject.js

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,11 +1,9 @@
 export default function iterateThroughObject(reportWithIterator) {
-  let st = '';
-  for (const idx of reportWithIterator) {
-    if (idx === reportWithIterator[0]) {
-      st = idx;
-        continue;
-    }
-    st = `${st} | ${idx}`
+  let n = reportWithIterator.next();
+  let res = '';
+  while (!n.done) {
+    res += `${n.value} | `;
+    n = reportWithIterator.next();
   }
-  return st;
+  return res.slice(0, res.length - 3);
 }
