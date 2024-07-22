4565a6cfc592c43e1682d3189366eb99c8fa2cf9
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-07-22 14:00:21 +0100

    Update 12-createReportObject.js

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,8 +1,14 @@
 export default function createReportObject(employeesList) {
+  const allEmployees = {};
+
+  for (const [department, employees] of Object.entries(employeesList)) {
+    allEmployees[department] = employees;
+  }
+
+  const getNumberOfDepartments = () => Object.keys(allEmployees).length;
+
   return {
-    allEmployees: employeesList,
-    getNumberOfDepartments(employeesList) {
-      return Object.keys(employeesList).length;
-    },
+    allEmployees,
+    getNumberOfDepartments,
   };
 }
