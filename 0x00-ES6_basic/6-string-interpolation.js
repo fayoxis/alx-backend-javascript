423ae607a823d51d15d08250c9b58fdcfc5844ed
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-07-22 13:38:46 +0100

    Update 6-string-interpolation.js

--- 0x00-ES6_basic/6-string-interpolation.js
+++ 0x00-ES6_basic/6-string-interpolation.js
@@ -1,12 +1,14 @@
-export default function getSanFranciscoDescription() {
-  const year = 2017;
+function getCurrentYear() {
+  const date = new Date();
+  return date.getFullYear();
+}
+
+export default function getBudgetForCurrentYear(income, gdp, capita) {
   const budget = {
-    income: '$119,868',
-    gdp: '$154.2 billion',
-    capita: '$178,479',
+    [`income-${getCurrentYear()}`]: income,
+    [`gdp-${getCurrentYear()}`]: gdp,
+    [`capita-${getCurrentYear()}`]: capita,
   };
 
-  return `As of ${year}, it was the seventh-highest income county in the United States\
-, with a per capita personal income of ${budget.income}. As of 2015, San Francisco \
-proper had a GDP of ${budget.gdp}, and a GDP per capita of ${budget.capita}.`;
+  return budget;
 }
