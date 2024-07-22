c5de44e7708e1ec055f36de973eba50127c9df22
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-07-22 13:08:26 +0100

    Update 1-block-scoped.js

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,13 @@
 export default function taskBlock(trueOrFalse) {
-  let task = false;
-  let task2 = true;
+  const task;
+  const task2;
 
   if (trueOrFalse) {
-   let task = true;
-   let task2 = false;
+    const task = true;
+    const task2 = false;
+  } else {
+    const task = false;
+    const task2 = true;
   }
 
   return [task, task2];
