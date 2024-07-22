e4a162372626531585b3f2f47d8a1241a2838121
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-07-22 12:36:33 +0100

    Update 0-constants.js

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,15 +1,14 @@
-export function taskFirst() {
+const taskFirst = () => {
   const task = 'I prefer const when I can.';
   return task;
-}
+};
 
-export function getLast() {
-  return ' is okay';
-}
+const getLast = () => ' is okay';
 
-export function taskNext() {
+const taskNext = () => {
   let combination = 'But sometimes let';
   combination += getLast();
-
   return combination;
-}
+};
+
+export { taskFirst, getLast, taskNext };
