e5391f372ef4e866470a44d1678e13adaf252aa3
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-07-24 13:37:52 +0100

    Update 8-try.js

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,7 +1,8 @@
-export default function divideFunction(numerator, denominator) {
-  if (denominator === 0) {
-    throw Error('cannot divide by 0');
-  } else {
-    return numerator / denominator;
+const divideFunction = (numerator, denominator) => {
+  if (!denominator) {
+    throw new Error('cannot divide by 0');
   }
-}
+  return numerator / denominator;
+};
+
+export default divideFunction;
