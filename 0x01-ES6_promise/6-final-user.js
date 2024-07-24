2b106c16116b9396a8980a09fe0fbadb783e834f
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-07-24 13:31:51 +0100

    Update 6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,12 +2,21 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise
-    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((res) => (
-      res.map((o) => ({
-        status: o.status,
-        value: o.status === 'fulfilled' ? o.value : String(o.reason),
-      }))
-    ));
+  try {
+    const [userSignup, photoUpload] = await Promise.allSettled([
+      signUpUser(firstName, lastName),
+      uploadPhoto(fileName),
+    ]);
+
+    return [userSignup, photoUpload].map((result) => {
+      const { status, value, reason } = result;
+      return {
+        status,
+        value: status === 'fulfilled' ? value : String(reason),
+      };
+    });
+  } catch (error) {
+    console.error('An error occurred during profile signup:', error);
+    throw error;
+  }
 }
