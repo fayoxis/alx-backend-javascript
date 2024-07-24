af17935d458ae124484a3f20a2e49491084b6323
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-07-24 12:02:25 +0100

    Update 0-promise.js

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,3 +1,13 @@
 export default function getResponseFromAPI() {
-  return new Promise(() => {});
+  
+  return new Promise((resolve, reject) => {
+    // You can add your API call logic here
+    // For example, you can use fetch or axios to make the API request
+    // and then resolve the Promise with the response data
+    // or reject it with an error message
+
+    // For now, let's simulate a successful API response
+    const mockResponse = { data: 'This is a mock response from the API' };
+    resolve(mockResponse);
+  });
 }
