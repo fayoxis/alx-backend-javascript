1faa37b9a5a6a9ffd8cef646418846d15119e55e
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-07-24 12:00:24 +0100

    Update 0-promise.js

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,5 +1,3 @@
 export default function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
-    resolve('Placeholder response from API');
-  });
+  return new Promise((resolve, reject) => {});
 }
