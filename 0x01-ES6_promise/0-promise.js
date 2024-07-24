93e9b43bfebb3ad6e24088212e0a7df84139ec2e
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-07-24 11:56:42 +0100

    Update 0-promise.js

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,10 +1,14 @@
 export default function getResponseFromAPI() {
+  
   return new Promise((resolve, reject) => {
+    
     // You can add your API call logic here
     // For example, you can use fetch or Axios to make the API request
     // and then call resolve() with the response data or reject() with an error
 
     // Resolve with a placeholder value for now
+    
     resolve('Placeholder response from API');
+    
   });
 }
