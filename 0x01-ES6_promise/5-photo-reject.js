f1ebea8e10f8a3dcf4c4de9205b932c4425596ee
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-07-24 13:24:27 +0100

    Update 5-photo-reject.js

--- 0x01-ES6_promise/5-photo-reject.js
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -1,3 +1,7 @@
-export default function uploadPhoto(filename) {
-  return Promise.reject(new Error(`${filename} cannot be processed`));
-}
+const uploadPhoto = filename => {
+  return new Promise((_, reject) => {
+    reject(new Error(`${filename} cannot be processed`));
+  });
+};
+
+export default uploadPhoto;
