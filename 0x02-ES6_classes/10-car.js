e4b78fa1d4546b08e36df382fed7c74314145462
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-07-25 18:39:29 +0100

    Update 10-car.js

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,19 +1,23 @@
-class Car {
-  #brand;
-  #motor;
-  #color;
-
+export default class Car {
+  // Constructor function to initialize the car object
   constructor(brand, motor, color) {
-    this.#brand = brand;
-    this.#motor = motor;
-    this.#color = color;
+    // Assign the brand parameter to the _brand property
+    this._brand = brand;
+    // Assign the motor parameter to the _motor property
+    this._motor = motor;
+    // Assign the color parameter to the _color property
+    this._color = color;
   }
 
-  static [Symbol.species] = this;
+  // Static method accessor using the Symbol.species well-known symbol
+  static [Symbol.species]() {
+    // Return the constructor function itself (Car)
+    return this;
+  }
 
+  // Method to create a new instance of the same class
   cloneCar() {
+    // Create a new instance of the class using the constructor returned by Symbol.species
     return new this.constructor[Symbol.species]();
   }
 }
-
-export default Car;
