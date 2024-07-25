a0cf04a13da291ae227abc8bf290882f5464b469
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-07-25 18:34:13 +0100

    Update 10-car.js

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -5,37 +5,12 @@ export default class Car {
     this.color = color;
   }
 
-  get brand() {
-    return this._brand;
-  }
-
-  set brand(value) {
-    this._brand = value;
-  }
-
-  get motor() {
-    return this._motor;
-  }
-
-  set motor(value) {
-    this._motor = value;
-  }
-
-  get color() {
-    return this._color;
-  }
-
-  set color(value) {
-    this._color = value;
-  }
-
-  static get [Symbol.species]() {
+  static [Symbol.species]() {
     return this;
   }
 
   cloneCar() {
-    const Species = this.constructor[Symbol.species];
-
-    return new Species();
+    const SpeciesConstructor = this.constructor[Symbol.species];
+    return new SpeciesConstructor();
   }
 }
