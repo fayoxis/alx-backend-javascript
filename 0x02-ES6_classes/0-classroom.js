4d5dc9846f9082dd59212e3426636aa01dd7e9ee
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-07-25 11:48:18 +0100

    Update 0-classroom.js

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,11 +1,11 @@
 /**
- * Represents a class room.
+ * this will represent a class room.
  */
 export default class ClassRoom {
   /**
-   * Creates a new @see {@link ClassRoom}.
-   * @param {Number} maxStudentsSize - The maximum number of
-   * students in the class.
+   * use this to Creates a new @see {@link ClassRoom}.
+   * @param {number} maxStudents - The maximum number of students
+   * allowed in the class.
    */
   constructor(maxStudentsSize) {
     this._maxStudentsSize = maxStudentsSize;
