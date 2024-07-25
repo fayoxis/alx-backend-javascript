01122d5310cf45b41d36719cf1cd05c2998045ac
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-07-25 17:59:51 +0100

    Update 4-pricing.js

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,9 +1,7 @@
-import Currency from './3-currency';
-
 export default class Pricing {
   constructor(amount, currency) {
-    this.amount = amount;
-    this.currency = currency;
+    this._amount = amount;
+    this._currency = currency;
   }
 
   get amount() {
@@ -11,39 +9,30 @@ export default class Pricing {
   }
 
   set amount(value) {
-    if (typeof value !== 'number') {
-      throw new TypeError('amount must be a number');
+    if (!Number.isFinite(value)) {
+      throw new TypeError('amount must be a finite number');
     }
     this._amount = value;
   }
 
-  /**
-   * @returns {Currency}
-   */
   get currency() {
     return this._currency;
   }
 
-  /**
-   * @param {Currency} value
-   */
   set currency(value) {
     if (!(value instanceof Currency)) {
-      throw new TypeError('currency must be a Currency');
+      throw new TypeError('currency must be an instance of Currency');
     }
     this._currency = value;
   }
 
   displayFullPrice() {
-    return `${this.amount} ${this.currency.name} (${this.currency.code})`;
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
   }
 
   static convertPrice(amount, conversionRate) {
-    if (typeof amount !== 'number') {
-      throw new TypeError('amount must be a number');
-    }
-    if (typeof conversionRate !== 'number') {
-      throw new TypeError('conversionRate must be a number');
+    if (!Number.isFinite(amount) || !Number.isFinite(conversionRate)) {
+      throw new TypeError('amount and conversionRate must be finite numbers');
     }
     return amount * conversionRate;
   }
