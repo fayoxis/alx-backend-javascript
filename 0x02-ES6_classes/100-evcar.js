32ca27f45b9e3be75dbb239dab2857527e963b95
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-07-25 18:45:07 +0100

    Update 100-evcar.js

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,39 +1,21 @@
 import Car from './10-car';
 
-// This is a class definition for an electric vehicle car (EVCar)
-// It extends the Car class, inheriting its properties and methods
 export default class EVCar extends Car {
-  // Constructor function that takes in brand, motor, color, and range as arguments
   constructor(brand, motor, color, range) {
-    // Call the parent class (Car) constructor with brand, motor, and color arguments
     super(brand, motor, color);
-    // Set the range property of the EVCar instance using the setRange method
-    this.setRange(range);
+    this._range = range;
   }
 
-  // Method to get the range of the EVCar instance
-  getRange() {
+  get range() {
     return this._range;
   }
 
-  // Method to set the range of the EVCar instance
-  setRange(value) {
-    this._range = value;
+  set range(newRange) {
+    this._range = newRange;
   }
 
-  // Method to create a new instance of the same class as the current instance
-  // This is done using the species pattern and the Symbol.species well-known symbol
   cloneCar() {
-    // Get the constructor of the derived class (EVCar or a class that extends EVCar)
-    const SpeciesConstructor = super.constructor[Symbol.species] || this.constructor;
-    // Create a new instance of the derived class using the SpeciesConstructor
-    return new SpeciesConstructor();
-  }
-}
-
-  cloneCar() {
-    const Species = super.constructor[Symbol.species];
-
-    return new Species();
+    const Constructor = Object.getPrototypeOf(this).constructor[Symbol.species] || this.constructor;
+    return new Constructor();
   }
 }
