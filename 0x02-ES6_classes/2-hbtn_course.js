1fd5105e807d081d319e10d318c99cb1a8c830ff
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-07-25 18:05:14 +0100

    Update 2-hbtn_course.js

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,7 +1,4 @@
-/**
- * Represents a course offered by a learning institution.
- */
-
+/* eslint-disable no-underscore-dangle */
 export default class HolbertonCourse {
   /**
    * Creates a new HolbertonCourse class
@@ -13,52 +10,61 @@ export default class HolbertonCourse {
    * Implement getters and setters for each attribute
    */
   constructor(name, length, students) {
+    // Initialize private properties with the provided values
     this._name = name;
     this._length = length;
     this._students = students;
   }
 
+  // Getter for the name property
   get name() {
     return this._name;
   }
 
-  /**
-   * Retrieves the course name.
-   * @returns {string} The course name.
-   */
+  // Setter for the name property
   set name(value) {
+    // Check if the provided value is a string
     if (typeof value !== 'string') {
       throw new TypeError('Name must be a string');
     }
     this._name = value;
   }
 
-  /**
-   * Retrieves the course duration in months.
-   * @returns {number} The course duration in months.
-   */
+  // Getter for the length property
   get length() {
     return this._length;
   }
 
+  // Setter for the length property
   set length(value) {
+    // Check if the provided value is a number
     if (typeof value !== 'number') {
       throw new TypeError('Length must be a number');
     }
     this._length = value;
   }
 
+  // Getter for the students property
   get students() {
     return this._students;
   }
 
+  // Setter for the students property
   set students(value) {
+    // Check if the provided value is an array
     if (!Array.isArray(value)) {
       throw new TypeError('Students must be an array');
     }
-    if (!value.every((student) => typeof student === 'string')) {
-      throw new TypeError('Students must be an array of strings');
+    
+    // Loop through each element in the array
+    for (const student of value) {
+      // Check if the element is a string
+      if (typeof student !== 'string') {
+        throw new TypeError('Students must be an array of strings');
+      }
     }
+    
+    // If all elements are strings, assign the array to the private property
     this._students = value;
   }
 }
