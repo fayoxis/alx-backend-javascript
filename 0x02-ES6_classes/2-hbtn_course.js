7faa2e7066de52c2922185bc4dda6baf13ef1777
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-07-25 18:35:48 +0100

    Update 2-hbtn_course.js

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,49 +1,66 @@
 export default class HolbertonCourse {
+  /**
+   * Creates a new HolbertonCourse class
+   *
+   * @param {String} name - The name of the course.
+   * @param {Number} length - How long the course is (in months).
+   * @param {String[]} students - The names of students in the course.
+   *
+   * Implement getters and setters for each attribute
+   */
   constructor(name, length, students) {
-    this._name = this.validateName(name);
-    this._length = this.validateLength(length);
-    this._students = this.validateStudents(students);
+    // Initialize instance properties using the provided arguments
+    this.name = name;
+    this.length = length;
+    this.students = students;
   }
 
-  validateName(name) {
-    if (typeof name !== 'string') throw TypeError('name must be a String');
-    return name;
-  }
-
-  validateLength(length) {
-    if (typeof length !== 'number') throw TypeError('length must be a Number');
-    return length;
-  }
-
-  validateStudents(students) {
-    if (!Array.isArray(students)) throw TypeError('students must be an Array');
-    for (const student of students) {
-      if (typeof student !== 'string') throw TypeError('students must contain strings');
-    }
-    return students;
-  }
-
-  set name(name) {
-    this._name = this.validateName(name);
-  }
-
+  // Getter for the 'name' property
   get name() {
     return this._name;
   }
 
-  set length(length) {
-    this._length = this.validateLength(length);
+  // Setter for the 'name' property
+  set name(value) {
+    // Check if the provided value is a string
+    if (typeof value !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    // Assign the provided value to the '_name' property
+    this._name = value;
   }
 
+  // Getter for the 'length' property
   get length() {
     return this._length;
   }
 
-  set students(students) {
-    this._students = this.validateStudents(students);
+  // Setter for the 'length' property
+  set length(value) {
+    // Check if the provided value is a number
+    if (typeof value !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    // Assign the provided value to the '_length' property
+    this._length = value;
   }
 
+  // Getter for the 'students' property
   get students() {
     return this._students;
+  }
+
+  // Setter for the 'students' property
+  set students(value) {
+    // Check if the provided value is an array
+    if (!(value instanceof Array)) {
+      throw new TypeError('Students must be an array of strings');
+    }
+    // Check if all elements in the array are strings
+    if (!value.every((student) => typeof student === 'string')) {
+      throw new TypeError('Students must be an array of strings');
+    }
+    // Assign the provided value to the '_students' property
+    this._students = value;
   }
 }
