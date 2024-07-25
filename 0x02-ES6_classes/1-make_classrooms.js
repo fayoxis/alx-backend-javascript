7ad6f25ed75343825ebb12806e66b436e332b51c
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-07-25 14:51:12 +0100

    Update 1-make_classrooms.js

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,10 +1,11 @@
 import ClassRoom from './0-classroom';
 
-/**
- * Creates an array of {@link ClassRoom}s with specific sizes.
- * @returns {ClassRoom[]} An array of {@link ClassRoom}s.
- */
-export default function initializeRooms() {
-  const sizes = [19, 20, 34];
-  return sizes.map(size => new ClassRoom(size));
-}
+const createClassRoom = (capacity) => new ClassRoom(capacity);
+
+const initializeRooms = () => [
+  createClassRoom(19),
+  createClassRoom(20),
+  createClassRoom(34),
+];
+
+export default initializeRooms;
