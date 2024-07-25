ac8f1d36db24d72951a1e6b7e094422aca6beeec
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-07-25 18:32:59 +0100

    Update 9-hoisting.js

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -1,35 +1,47 @@
-export default class HolbertonClass {
-  #size;
-  #location;
-
-  constructor(size, location) {
-    this.#size = size;
-    this.#location = location;
+export class HolbertonClass {
+  constructor(year, location) {
+    this._year = year;
+    this._location = location;
   }
 
-  set size(value) {
-    this.#size = value;
-  }
-
-  get size() {
-    return this.#size;
-  }
-
-  set location(value) {
-    this.#location = value;
+  get year() {
+    return this._year;
   }
 
   get location() {
-    return this.#location;
-  }
-
-  [Symbol.toPrimitive](hint) {
-    if (hint === 'string') {
-      return this.location;
-    }
-    if (hint === 'number') {
-      return this.size;
-    }
-    return null;
+    return this._location;
   }
 }
+
+export class StudentHolberton {
+  constructor(firstName, lastName, holbertonClass) {
+    this._firstName = firstName;
+    this._lastName = lastName;
+    this._holbertonClass = holbertonClass;
+  }
+
+  get fullName() {
+    return `${this._firstName} ${this._lastName}`;
+  }
+
+  get holbertonClass() {
+    return this._holbertonClass;
+  }
+
+  get fullStudentDescription() {
+    return `${this.fullName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
+  }
+}
+
+const class2019 = new HolbertonClass(2019, 'San Francisco');
+const class2020 = new HolbertonClass(2020, 'San Francisco');
+
+const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
+const student2 = new StudentHolberton('John', 'Doe', class2020);
+const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
+const student4 = new StudentHolberton('Donald', 'Bush', class2019);
+const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
+
+const listOfStudents = [student1, student2, student3, student4, student5];
+
+export default listOfStudents;
