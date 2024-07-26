cdbacc3f45d4550fa55dba7697e26e555780214b
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-07-26 16:44:33 +0100

    Update 2-hbtn_course.js

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,66 +1,71 @@
+/**
+ *  shows the Holberton Course on.
+ */
 export default class HolbertonCourse {
   /**
-   * Creates a new HolbertonCourse class
+   * determine a new @see {@link HolbertonCourse}.
    *
-   * @param {String} name - The name of the course.
-   * @param {Number} length - How long the course is (in months).
-   * @param {String[]} students - The names of students in the course.
-   *
-   * Implement getters and setters for each attribute
+   * @param {String} name -  name of  course.
+   * @param {Number} length - How the course is (in months).
+   * @param {String[]} students -  names of most students in the course.
    */
   constructor(name, length, students) {
-    // Initialize instance properties using the provided arguments
     this.name = name;
     this.length = length;
     this.students = students;
   }
 
-  // Getter for the 'name' property
+  /**
+   *  name of this course.
+   */
   get name() {
     return this._name;
   }
 
-  // Setter for the 'name' property
+  /**
+   * Sets the  course.
+   */
   set name(value) {
-    // Check if the provided value is a string
     if (typeof value !== 'string') {
       throw new TypeError('Name must be a string');
     }
-    // Assign the provided value to the '_name' property
     this._name = value;
   }
 
-  // Getter for the 'length' property
+  /**
+   * Gets the length of course.
+   */
   get length() {
     return this._length;
   }
 
-  // Setter for the 'length' property
+  /**
+   * Sets the length (in months).
+   */
   set length(value) {
-    // Check if the provided value is a number
     if (typeof value !== 'number') {
       throw new TypeError('Length must be a number');
     }
-    // Assign the provided value to the '_length' property
     this._length = value;
   }
 
-  // Getter for the 'students' property
+  /**
+   * Gets the names of students.
+   */
   get students() {
     return this._students;
   }
 
-  // Setter for the 'students' property
+  /**
+   * Sets the names of students .
+   */
   set students(value) {
-    // Check if the provided value is an array
     if (!(value instanceof Array)) {
       throw new TypeError('Students must be an array of strings');
     }
-    // Check if all elements in the array are strings
     if (!value.every((student) => typeof student === 'string')) {
       throw new TypeError('Students must be an array of strings');
     }
-    // Assign the provided value to the '_students' property
     this._students = value;
   }
 }
