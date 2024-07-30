2beb1cb31b5c57de3817ea04b075de567370a473
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-07-30 22:15:07 +0100

    Update 2-get_students_by_loc.js

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -6,7 +6,7 @@
  *   location: String
  * }[]} students - The list of students.
  * @param {String} city - The location.
- * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ * @author <https://github.com/>
  * @returns
  */
 export default function getStudentsByLocation(students, city) {
