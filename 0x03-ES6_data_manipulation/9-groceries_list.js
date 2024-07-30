722f57511a603aa7df9ef24b51f238979aa834eb
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-07-30 22:41:38 +0100

    Update 9-groceries_list.js

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,15 +1,15 @@
 /**
- * Retrieves a map of grocery names and their quantity.
- * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ * Creates and returns a Map of grocery items and their quantities.
+ * @author <https://github.com/>
  * @returns {Map<string, Number>}
  */
-export default function groceriesList() {
-  const values = [
+export default function getGroceryInventory() {
+  const groceryItems = [
     ['Apples', 10],
     ['Tomatoes', 10],
     ['Pasta', 1],
     ['Rice', 1],
     ['Banana', 5],
   ];
-  return new Map(values);
+  return new Map(groceryItems);
 }
