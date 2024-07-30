8329ad5d637a91e161f32c4f973224a6c853222b
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-07-30 22:43:09 +0100

    Update 8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,7 +2,6 @@ export default function cleanSet(set, startString) {
   if (!set || !startString || !(set instanceof Set) || typeof startString !== 'string') {
     return '';
   }
-  
   return Array.from(set)
     .filter(value => typeof value === 'string' && value.startsWith(startString))
     .map(value => value.slice(startString.length))
