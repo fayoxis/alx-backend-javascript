56018c69f23fac64693e1552a918b8ea5f8f4162
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-07-30 22:47:21 +0100

    Update 8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,9 +2,13 @@ export default function cleanSet(set, startString) {
   if (!set || !startString || !(set instanceof Set) || typeof startString !== 'string') {
     return '';
   }
+
   return Array.from(set)
-    .filter(value => typeof value === 'string' && value.startsWith(startString))
+    .filter(value => 
+      typeof value === 'string' && 
+      value.startsWith(startString) && 
+      value.length > startString.length
+    )
     .map(value => value.slice(startString.length))
-    .filter(Boolean)
     .join('-');
 }
