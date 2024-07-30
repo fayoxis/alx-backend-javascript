c7e301e89f6fea48be59c1d3c8390b3ac291de60
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-07-30 23:01:27 +0100

    Update 10-update_uniq_items.js

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -4,13 +4,9 @@
  * grocery and its quantity.
  * @author <https://github.com/>
  */
-export default function updateUniqueItems(map) {
-  if (!Map.prototype.isPrototypeOf(map)) {
-    throw new Error('Cannot process');
-  }
-  for (const [key, value] of map.entries()) {
-    if (value === 1) {
-      map.set(key, 100);
-    }
-  }
+export default function updateUniqueItems(olMap) {
+  if (!(olMap instanceof Map)) throw Error('Cannot process');
+  olMap.forEach((value, key) => {
+    if (value === 1) olMap.set(key, 100);
+  });
 }
