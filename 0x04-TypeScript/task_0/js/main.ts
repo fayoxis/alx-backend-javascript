9ccd96677755af2d989a956f7b4ae1abe39ef023
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-07-31 20:46:54 +0100

    Update main.ts

--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -21,38 +21,7 @@ const studentsList: Array<Student> = [
   studentA,
   studentB,
 ];
-const styleSheet = `
-  html {
-    margin: 0;
-    height: 100%;
-  }
-  body {
-    box-sizing: border-box;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    height: 80%;
-    margin: 10%;
-  }
-  table {
-    border-collapse: collapse;
-  }
-  thead {
-    font-weight: bold;
-  }
-  td {
-    padding: 10px;
-    border: 1px solid gray;
-    cursor: pointer;
-  }
-  td:hover {
-    background: gainsboro;
-  }
 
-  td:nth-child(1) {
-    text-align: center;
-  }
-`;
 
 export const displayStudents = (students: Array<Student>): void => {
   const table = document.createElement('table');
