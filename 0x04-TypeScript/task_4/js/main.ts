82de96878a8e712e1286553a1d333bc779d7cdd1
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-07-31 23:58:20 +0100

    Update main.ts

--- 0x04-TypeScript/task_4/js/main.ts
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -1,23 +1,64 @@
-export const cpp: Subjects.Cpp = new Subjects.Cpp();
-export const java: Subjects.Java = new Subjects.Java();
-export const react: Subjects.React = new Subjects.React();
-export const cTeacher: Subjects.Teacher = {
+interface Teacher {
+  firstName: string;
+  lastName: string;
+  experienceTeachingC: number;
+}
+
+class Subject {
+  private teacher: Teacher | null = null;
+
+  setTeacher(teacher: Teacher) {
+    this.teacher = teacher;
+  }
+
+  getRequirements(): string {
+    return 'Default requirements';
+  }
+
+  getAvailableTeacher(): string {
+    return this.teacher ? `Available Teacher: ${this.teacher.firstName} ${this.teacher.lastName}` : 'No available teacher';
+  }
+}
+
+class Cpp extends Subject {
+  getRequirements(): string {
+    return 'Here is the list of requirements for C++';
+  }
+}
+
+class Java extends Subject {
+  getRequirements(): string {
+    return 'Here is the list of requirements for Java';
+  }
+}
+
+class React extends Subject {
+  getRequirements(): string {
+    return 'Here is the list of requirements for React';
+  }
+}
+
+const cpp = new Cpp();
+const java = new Java();
+const react = new React();
+
+const cTeacher: Teacher = {
   firstName: 'Dennis',
   lastName: 'Ritchie',
   experienceTeachingC: 10,
 };
 
 console.log('C++');
-cpp.setTeacher = cTeacher;
+cpp.setTeacher(cTeacher);
 console.log(cpp.getRequirements());
 console.log(cpp.getAvailableTeacher());
 
 console.log('Java');
-java.setTeacher = cTeacher;
+java.setTeacher(cTeacher);
 console.log(java.getRequirements());
 console.log(java.getAvailableTeacher());
 
 console.log('React');
-react.setTeacher = cTeacher;
+react.setTeacher(cTeacher);
 console.log(react.getRequirements());
 console.log(react.getAvailableTeacher());
