dcc0227aa64b33cc3109f0acbf7850488abaf77d
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-07-31 23:39:37 +0100

    Update main.ts

--- 0x04-TypeScript/task_5/js/main.ts
+++ 0x04-TypeScript/task_5/js/main.ts
@@ -1,15 +1,17 @@
+type BrandedNumber<T extends string> = number & { __brand: T };
+
 export interface MajorCredits {
-  credits: number & { __brand: 'MajorCredits.credits' };
+  credits: BrandedNumber<'MajorCredits'>;
 }
 
 export interface MinorCredits {
-  credits: number & { __brand: 'MinorCredits.credits' };
+  credits: BrandedNumber<'MinorCredits'>;
 }
 
 export function sumMajorCredits(subject1: MajorCredits, subject2: MajorCredits): MajorCredits {
-  return { credits: subject1.credits + subject2.credits } as MajorCredits;
+  return { credits: (subject1.credits + subject2.credits) as BrandedNumber<'MajorCredits'> };
 }
 
 export function sumMinorCredits(subject1: MinorCredits, subject2: MinorCredits): MinorCredits {
-  return { credits: subject1.credits + subject2.credits } as MinorCredits;
+  return { credits: (subject1.credits + subject2.credits) as BrandedNumber<'MinorCredits'> };
 }
