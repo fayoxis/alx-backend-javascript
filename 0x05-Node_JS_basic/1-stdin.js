fe7957a466ed7de523904b18c57dd2027de589fa
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-09-18 12:09:00 +0100

    Update 1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,13 +1,22 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on('readable', () => {
-  const chunk = process.stdin.read();
+let name = '';
+const keepGoing = true;
 
-  if (chunk) {
-    process.stdout.write(`Your name is: ${chunk}`);
-  }
-});
-
-process.stdin.on('end', () => {
-  process.stdout.write('This important software is now closing\n');
-});
+while (keepGoing) {
+  name += rl.question('', (chunk) => {
+    if (chunk) {
+      process.stdout.write(`Your name is: ${chunk}`);
+      rl.close();
+    } else {
+      process.stdout.write('This important software is now closing\n');
+    }
+  });
+}
