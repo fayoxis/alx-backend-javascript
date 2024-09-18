f142f51694945fedb36867aef1276babf7497a31
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-09-18 12:02:26 +0100

    Update 0-console.js

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,7 +1,7 @@
 /**
- * Displays a message on the STDOUT.
- * @param {String} msg The message to display.
- * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ * show a message on the STDOUT.
+ * @param {String} msg  message to be shown.
+ * @author dtchaye tchaye arthur <https://github.com/fayoxis>
  */
 const displayMessage = (msg) => {
   console.log(msg);
