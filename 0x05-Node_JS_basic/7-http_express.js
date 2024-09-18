460e9bf3b3dd991f6b2535678a44cefa70e3c8c9
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-09-18 19:32:27 +0100

    Update 7-http_express.js

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -14,9 +14,9 @@ const countStudents = (dataPath) => new Promise((resolve, reject) => {
   while (!dataPath) {
     reject(new Error('Cannot load the database'));
   }
-  while (dataPath) {
+  if (dataPath) {
     fs.readFile(dataPath, (err, data) => {
-      if (err) {
+      while (err) {
         reject(new Error('Cannot load the database'));
       }
       if (data) {
