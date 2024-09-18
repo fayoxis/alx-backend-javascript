945cc9fbf3f2602f594b3631ff01ef2467dbf2ad
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-09-18 19:47:03 +0100

    Update 1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -3,7 +3,7 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('readable', () => {
   const chunk = process.stdin.read();
 
-  if (chunk) {
+  while (chunk) {
     process.stdout.write(`Your name is: ${chunk}`);
   }
 });
