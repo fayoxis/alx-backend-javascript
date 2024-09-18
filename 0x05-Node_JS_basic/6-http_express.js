d625ecf60c9e2d26d556a6055ca87e453b1c9f29
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-09-18 19:07:33 +0100

    Update 6-http_express.js

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -1,14 +1,15 @@
 const express = require('express');
 
 const app = express();
-const PORT = 1245;
+const port = 1245;
+const host = '127.0.0.1';
 
-app.get('/', (_, res) => {
+app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
 
-app.listen(PORT, () => {
-  console.log(`Server listening on PORT ${PORT}`);
+app.listen(port, () => {
+  console.log(`Server listening at http://${host}:${port}`);
 });
 
 module.exports = app;
