75c1271a293411cf9eb0c6e89ff58dcdc5e6ab0c
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-09-18 19:39:06 +0100

    Update server.js

--- 0x05-Node_JS_basic/full_server/server.js
+++ 0x05-Node_JS_basic/full_server/server.js
@@ -1,13 +1,15 @@
-import express from 'express';
-import mapRoutes from './routes';
+const express = require('express');
+const router = require('./routes/index');
 
 const app = express();
-const PORT = 1245;
+const port = 1245;
 
-mapRoutes(app);
-app.listen(PORT, () => {
-  console.log(`Server listening on PORT ${PORT}`);
+app.use('/', router);
+app.use('/students', router);
+app.use('/students/:major', router);
+
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`);
 });
 
-export default app;
 module.exports = app;
