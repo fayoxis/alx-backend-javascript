44dd3dda679f419fb9c8793c6482176f89ffdc9a
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-09-18 12:59:01 +0100

    Update 3-read_file_async.js

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -11,31 +11,26 @@ const countStudents = (dataPath) => new Promise((resolve, reject) => {
       reject(new Error('Cannot load the database'));
     }
     if (data) {
-      const fileLines = data
-        .toString('utf-8')
-        .trim()
-        .split('\n');
+      const fileLines = data.toString('utf-8').trim().split('\n');
       const studentGroups = {};
       const dbFieldNames = fileLines[0].split(',');
-      const studentPropNames = dbFieldNames
-        .slice(0, dbFieldNames.length - 1);
+      const studentPropNames = dbFieldNames.slice(0, dbFieldNames.length - 1);
 
-      for (const line of fileLines.slice(1)) {
+      let lineIndex = 1;
+      while (lineIndex < fileLines.length) {
+        const line = fileLines[lineIndex];
         const studentRecord = line.split(',');
-        const studentPropValues = studentRecord
-          .slice(0, studentRecord.length - 1);
+        const studentPropValues = studentRecord.slice(0, studentRecord.length - 1);
         const field = studentRecord[studentRecord.length - 1];
         if (!Object.keys(studentGroups).includes(field)) {
           studentGroups[field] = [];
         }
-        const studentEntries = studentPropNames
-          .map((propName, idx) => [propName, studentPropValues[idx]]);
+        const studentEntries = studentPropNames.map((propName, idx) => [propName, studentPropValues[idx]]);
         studentGroups[field].push(Object.fromEntries(studentEntries));
+        lineIndex++;
       }
 
-      const totalStudents = Object
-        .values(studentGroups)
-        .reduce((pre, cur) => (pre || []).length + cur.length);
+      const totalStudents = Object.values(studentGroups).reduce((pre, cur) => (pre || []).length + cur.length, 0);
       console.log(`Number of students: ${totalStudents}`);
       for (const [field, group] of Object.entries(studentGroups)) {
         const studentNames = group.map((student) => student.firstname).join(', ');
