664723df0c12e0679798adc58969c11d6be57d27
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-09-20 11:57:21 +0100

    Update 4-payment.js

--- 0x06-unittests_in_js/4-payment.js
+++ 0x06-unittests_in_js/4-payment.js
@@ -1,7 +1,7 @@
-const { calculateNumber } = require('./utils');
+const utils = require('./utils');
 
 module.exports = function sendPaymentRequestToApi(totalAmount, totalShipping) {
-  const total = calculateNumber('SUM', totalAmount, totalShipping);
+  const total = utils.calculateNumber('SUM', totalAmount, totalShipping);
   console.log(`The total is: ${total}`);
   return total;
 };
