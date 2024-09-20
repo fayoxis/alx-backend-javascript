de22234b2d491c3f5b29377123d4907cca569c77
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-09-20 13:02:14 +0100

    Update 5-payment.test.js

--- 0x06-unittests_in_js/5-payment.test.js
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -5,23 +5,19 @@ const sendPaymentRequestToApi = require('./5-payment');
 describe('sendPaymentRequestToApi', () => {
   let consoleSpy;
 
-  beforeEach(() => {
-    consoleSpy = sinon.spy(console, 'log');
-  });
+  beforeEach(() => consoleSpy = sinon.spy(console, 'log'));
+  afterEach(() => consoleSpy.restore());
 
-  afterEach(() => {
-    consoleSpy.restore();
-  });
+  const testCases = [
+    { args: [100, 20], expected: 'The total is: 120' },
+    { args: [10, 10], expected: 'The total is: 20' }
+  ];
 
-  it('sendPaymentRequestToApi(100, 20) logs "The total is: 120" to the console', () => {
-    sendPaymentRequestToApi(100, 20);
-    expect(consoleSpy.calledWith('The total is: 120')).to.be.true;
-    expect(consoleSpy.calledOnce).to.be.true;
-  });
-
-  it('sendPaymentRequestToApi(10, 10) logs "The total is: 20" to the console', () => {
-    sendPaymentRequestToApi(10, 10);
-    expect(consoleSpy.calledWith('The total is: 20')).to.be.true;
-    expect(consoleSpy.calledOnce).to.be.true;
+  testCases.forEach(({ args, expected }) => {
+    it(`logs "${expected}" for (${args.join(', ')})`, () => {
+      sendPaymentRequestToApi(...args);
+      expect(consoleSpy.calledWith(expected)).to.be.true;
+      expect(consoleSpy.calledOnce).to.be.true;
+    });
   });
 });
