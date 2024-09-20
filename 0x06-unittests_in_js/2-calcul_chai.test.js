a37c17b9d02a008ee5f6a1a260ba8df5196d8086
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-09-20 12:49:10 +0100

    Update 2-calcul_chai.test.js

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,59 +1,61 @@
-const {expect} = require('chai');
+const { expect } = require('chai');
 const calculateNumber = require('./2-calcul_chai');
 
 describe('calculateNumber', () => {
-  describe('SUM', () => {
-    it.each`
-      a     | b     | expected
-      ${2.0}| ${2.0}| ${4}
-      ${2.3}| ${1.8}| ${4}
-      ${-2.0}|${-2.0}|${-4}
-      ${-2.3}|${-1.8}|${-4}
-      ${-2.0}|${2.0} | ${0}
-      ${2.0}|${-2.0}| ${0}
-      ${0.0}| ${0.0}| ${0}
-    `('returns $expected for $a + $b', ({a, b, expected}) => {
+  describe('type == "SUM"', () => {
+    it.each([
+      [2.0, 2.0, 4],
+      [2.3, 1.8, 4],
+      [-2.0, -2.0, -4],
+      [-2.3, -1.8, -4],
+      [-2.0, 2.0, 0],
+      [2.0, -2.0, 0],
+      [0.0, 0.0, 0]
+    ])('Sum: %f + %f = %i', (a, b, expected) => {
       expect(calculateNumber('SUM', a, b)).to.equal(expected);
     });
   });
 
-  describe('SUBTRACT', () => {
-    it.each`
-      a     | b     | expected
-      ${2.0}| ${2.0}| ${0}
-      ${2.3}| ${1.8}| ${0}
-      ${-2.0}|${-2.0}|${0}
-      ${-2.3}|${-1.8}|${0}
-      ${-2.0}|${2.0} |${-4.0}
-      ${2.0}|${-2.0}|${4.0}
-      ${0.0}| ${0.0}| ${0}
-    `('returns $expected for $a - $b', ({a, b, expected}) => {
+  describe('type == "SUBTRACT"', () => {
+    it.each([
+      [2.0, 2.0, 0],
+      [2.3, 1.8, 0],
+      [-2.0, -2.0, 0],
+      [-2.3, -1.8, 0],
+      [-2.0, 2.0, -4.0],
+      [2.0, -2.0, 4.0],
+      [0.0, 0.0, 0]
+    ])('Subtract: %f - %f = %f', (a, b, expected) => {
       expect(calculateNumber('SUBTRACT', a, b)).to.equal(expected);
     });
   });
 
-  describe('DIVIDE', () => {
-    it.each`
-      a      | b      | expected
-      ${8.0} | ${2.0} | ${4.0}
-      ${-7.0}| ${2.0} | ${-3.5}
-      ${7.0} |${-2.0} | ${-3.5}
-      ${-7.0}|${-2.0} | ${3.5}
-      ${2.0} | ${2.0} | ${1}
-      ${-2.0}|${-2.0} | ${1}
-      ${2.6} | ${3.0} | ${1}
-      ${2.4} | ${2.0} | ${1}
-      ${0.0} | ${5.0} | ${0}
-      ${0.0} |${-5.0} | ${-0}
-      ${5.0} | ${0}   | ${'Error'}
-      ${5.0} | ${0.2} | ${'Error'}
-      ${5.0} |${-0.2} | ${'Error'}
-      ${-5.0}| ${0}   | ${'Error'}
-      ${-5.0}| ${0.2} | ${'Error'}
-      ${-5.0}|${-0.2} | ${'Error'}
-      ${0.0} | ${0.0} | ${'Error'}
-    `('returns $expected for $a / $b', ({a, b, expected}) => {
+  describe('type == "DIVIDE"', () => {
+    it.each([
+      [8.0, 2.0, 4.0],
+      [-7.0, 2.0, -3.5],
+      [7.0, -2.0, -3.5],
+      [-7.0, -2.0, 3.5],
+      [2.0, 2.0, 1],
+      [-2.0, -2.0, 1],
+      [2.6, 3.0, 1],
+      [2.4, 2.0, 1],
+      [0.0, 5.0, 0],
+      [0.0, -5.0, -0]
+    ])('Divide: %f / %f = %f', (a, b, expected) => {
       expect(calculateNumber('DIVIDE', a, b)).to.equal(expected);
+    });
+
+    it.each([
+      [5.0, 0],
+      [5.0, 0.2],
+      [5.0, -0.2],
+      [-5.0, 0],
+      [-5.0, 0.2],
+      [-5.0, -0.2],
+      [0.0, 0.0]
+    ])('Divide: %f / %f = "Error"', (a, b) => {
+      expect(calculateNumber('DIVIDE', a, b)).to.equal('Error');
     });
   });
 });
