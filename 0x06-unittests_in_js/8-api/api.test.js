07bd723adbc0535226b5c4db74bcbabfe43670aa
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-09-20 12:16:51 +0100

    Update api.test.js

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -6,8 +6,8 @@ describe('API integration test', () => {
 
   it('GET / returns correct response', (done) => {
     request.get(`${API_URL}/`, (_err, res, body) => {
-      expect(res.statusCode).to.be.equal(200);
-      expect(body).to.be.equal('Welcome to the payment system');
+      expect(res.statusCode).to.equal(200);
+      expect(body).to.equal('Welcome to the payment system');
       done();
     });
   });
