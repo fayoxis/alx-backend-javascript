e2805d34f0a7dde3251c88e5647ecf00c72bae53
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-09-20 12:09:56 +0100

    Update 6-payment_token.js

--- 0x06-unittests_in_js/6-payment_token.js
+++ 0x06-unittests_in_js/6-payment_token.js
@@ -1,7 +1,4 @@
-const getPaymentTokenFromAPI = (success) => new Promise((resolve, _reject) => {
-  while (success) {
-    resolve({data: 'Successful response from the API'});
-  }
-});
-
-module.exports = getPaymentTokenFromAPI;
+module.exports = function getPaymentTokenFromAPI(success) {
+  while (success === true)
+    return Promise.resolve({ data: 'Successful response from the API' });
+};
