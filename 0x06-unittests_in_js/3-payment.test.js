884244d263c266330cbe9fe1bf9641e16c0ed66f
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-09-20 11:54:50 +0100

    Update 3-payment.test.js

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,15 +1,15 @@
 const sinon = require('sinon');
-const Utils = require('./utils');
+const { calculateNumber } = require('./utils');
 const { expect } = require('chai');
 const sendPaymentRequestToApi = require('./3-payment');
 
 describe('sendPaymentRequestToApi', () => {
-  it('sendPaymentRequestToApi uses the calculateNumber method of Utils', () => {
-    const bigBrother = sinon.spy(Utils);
+  it('sendPaymentRequestToApi uses the calculateNumber function', () => {
+    const calculateNumberSpy = sinon.spy(calculateNumber);
 
     sendPaymentRequestToApi(100, 20);
-    expect(bigBrother.calculateNumber.calledWith('SUM', 100, 20)).to.be.true;
-    expect(bigBrother.calculateNumber.callCount).to.be.equal(1);
-    bigBrother.calculateNumber.restore();
+    expect(calculateNumberSpy.calledWith('SUM', 100, 20)).to.be.true;
+    expect(calculateNumberSpy.callCount).to.be.equal(1);
+    calculateNumberSpy.restore();
   });
 });
