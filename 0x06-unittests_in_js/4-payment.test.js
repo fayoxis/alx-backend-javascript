ce85d594b1dc95aee787125bf2f15a58f66ed120
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-09-20 11:58:06 +0100

    Update 4-payment.test.js

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -4,17 +4,19 @@ const { expect } = require('chai');
 const sendPaymentRequestToApi = require('./4-payment');
 
 describe('sendPaymentRequestToApi', () => {
-  it('sendPaymentRequestToApi calls console.log with the right arguments', () => {
-    const bigBrother = sinon.spy(console);
-    const dummy = sinon.stub(Utils, 'calculateNumber');
+  it('calls console.log with the correct arguments', () => {
+    const consoleSpy = sinon.spy(console, 'log');
+    const calculateNumberStub = sinon.stub(Utils, 'calculateNumber');
 
-    dummy.returns(10);
+    calculateNumberStub.returns(10);
     sendPaymentRequestToApi(100, 20);
-    expect(dummy.calledWith('SUM', 100, 20)).to.be.true;
-    expect(dummy.callCount).to.be.equal(1);
-    expect(bigBrother.log.calledWith('The total is: 10')).to.be.true;
-    expect(bigBrother.log.callCount).to.be.equal(1);
-    dummy.restore();
-    bigBrother.log.restore();
+
+    expect(calculateNumberStub.calledWith('SUM', 100, 20)).to.be.true;
+    expect(calculateNumberStub.callCount).to.be.equal(1);
+    expect(consoleSpy.calledWith('The total is: 10')).to.be.true;
+    expect(consoleSpy.callCount).to.be.equal(1);
+
+    calculateNumberStub.restore();
+    consoleSpy.restore();
   });
 });
